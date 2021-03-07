eac80bc33a34cadd2461c80b3631ffb730a971cd
Leonardo Moya <leonardo.moyars@udlap.mx>
2021-03-07 16:35:10 -0600

    added gunicorn dependency to run flask

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def profiles():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(threaded=True, port=5000)
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 python3 -m pip install -r requirements.txt
-python3 seed.py
-export FLASK_APP=server.py
-flask run
+python3 app.py
+gunicorn app:app
